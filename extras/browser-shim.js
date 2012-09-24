ccfca3ac7a59fa503d20178d80a4d7b83b5bc00c
R. S. Doiel <rsdoiel@gmail.com>
2012-09-24 07:47:51 -0700

    Added comment to browser-shim.js

--- extras/browser-shim.js
+++ extras/browser-shim.js
@@ -1,3 +1,12 @@
+//
+// browser-shim.js - add just enough to allow testing in browser.
+//
+// @author: R. S. Doiel, <rsdoiel@gmail.com>
+// copyright (c) 2012 all rights reserved
+//
+// Released under the Simplified BSD License.
+// See: http://opensource.org/licenses/bsd-license.php
+//
 // These are defined to stop errors in Chrome.
 // They are not used in browser based tests.
 (function (global, undefined) {
